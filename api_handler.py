38c0de94cd2c44f26f86e478477f398de59798d1
Владислав Ястребов <vladyastreboc@gmail.com>
2021-02-16 17:09:25 +0700

    Первый этап

---
+++ api_handler.py
@@ -0,0 +1,58 @@
+from io import BytesIO
+import requests
+from PIL import Image
+from config import *
+
+
+def get_object_by_address(address: str):
+    request_params = {
+        'apikey': GEOCODE_API_KEY,
+        'geocode': address,
+        'format': "json"
+    }
+    response = requests.get(GEOCODE_SERVER, params=request_params)
+
+    if not response:
+        raise RuntimeError(f"Ошибка выполнения запроса:\n"
+                           f"{response.url}\n"
+                           f"Статус: {response.status_code} {response.reason}\n"
+                           f"{response.content}")
+
+    data = response.json()
+    features = data['response']['GeoObjectCollection']['featureMember']
+    return features[0]['GeoObject'] if features else None
+
+
+def get_coordinates_by_address(address: str):
+    toponym = get_object_by_address(address)
+    if toponym is None:
+        return (None, None)
+
+    toponym_coordinates = toponym['Point']['pos']
+    toponym_longitude, toponym_lattitude = toponym_coordinates.split()
+
+    return (float(toponym_longitude), float(toponym_lattitude))
+
+
+def get_static_map_image(ll, points=tuple(), spn=None,
+        placemark="ya_en", mode="map"):
+    request_params = {
+        'll': ll,
+        'l': mode,
+    }
+    
+    if points:
+        request_params['pt'] = "~".join([",".join(point) for point in points])
+
+    if spn:
+        request_params['spn'] = spn
+
+    response = requests.get(STATIC_MAP_SERVER, params=request_params)
+
+    if not response:
+        raise AssertionError(f"Ошибка при получении статического изображения:\n"
+                             f"{response.url}\n"
+                             f"{response.status_code} {response.reason}\n"
+                             f"{response.content}")
+
+    return Image.open(BytesIO(response.content))
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
+from PIL.ImageQt import ImageQt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+from config import *
+from api_handler import *
 
 
 class MainWindow(QMainWindow):
@@ -8,6 +12,20 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi("ui_files/main_window.ui", self)
 
+        self.current_spn = INITIAL_SPN
+
+        self.show_location(INITIAL_LOCATION)
+
+    def show_location(self, location_name):
+        location_coordinates = get_coordinates_by_address(location_name)
+        location_ll = ",".join(map(str, location_coordinates))
+        spn = ",".join(map(str, self.current_spn))
+        print(location_ll, spn)
+        image = get_static_map_image(location_ll, spn=spn)
+
+        image = QPixmap.fromImage(ImageQt(image))
+
+        self.image_label.setPixmap(image)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
